e4680aec099fee2819b25ff1f259efe9eb219e5d
Laercio Simoes <laercio@hpcbrasil.com>
2024-07-30 11:58:05 -0300

    New Structure

--- src/app.tsx
+++ src/app.tsx
@@ -3,24 +3,24 @@ import { addNativeElement } from "@canva/design";
 import styles from "styles/components.css";
 
 export const App = () => {
-  const onClick = () => {
-    addNativeElement({
-      type: "TEXT",
-      children: ["We Find Video"],
-    });
-  };
+    const onClick = () => {
+        addNativeElement({
+            type: "TEXT",
+            children: ["Hello world!"],
+        });
+    };
 
-  return (
-    <div className={styles.scrollContainer}>
-      <Rows spacing="2u">
-        <Text>
-          To make changes to this app, edit the <code>src/app.tsx</code> file,
-          then close and reopen the app in the editor to preview the changes.
-        </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
-        </Button>
-      </Rows>
-    </div>
-  );
+    return (
+        <div className={styles.scrollContainer}>
+            <Rows spacing="2u">
+                <Text>
+                    To make changes to this app, edit the <code>src/app.tsx</code> file,
+                    then close and reopen the app in the editor to preview the changes.
+                </Text>
+                <Button variant="primary" onClick={onClick} stretch>
+                    Do something cool
+                </Button>
+            </Rows>
+        </div>
+    );
 };
